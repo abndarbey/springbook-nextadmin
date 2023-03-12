09b70f6f12e6cd09caca98e98f2b448d38886c99
Tiger Darbey <tiger@pop-os.localdomain>
2023-03-12 09:46:02 +0530

    seperate out html components for sku new

---
+++ src/modules/inventory/skus/SkuNew/SkuNewHTML.tsx
@@ -0,0 +1,88 @@
+import { Fragment, useState } from "react"
+import Page from "components/Page"
+
+import PageHeader from "components/PageHeader"
+import FormCard from "components/FormCard"
+import OrgSelectModal from "common/select-table/OrgSelectModal"
+import SkuCatalogueSelectModal from "common/select-table/SkuCatalogueSelectModal"
+import { INavTrailProps } from "components/NavTrails"
+import { Auther, SkuCatalogue, Organization } from "@lib/generated/hooks"
+import { UseFormReturnType } from "@mantine/form"
+import { TextInput } from "@mantine/core"
+
+interface ISkuFormValues {
+    skuUID: string,
+    orgUID: string,
+
+    orgName: string,
+    skuName: string,
+}
+
+interface ISkuNewHTML {
+    title: string
+    auther: Auther
+    orgUID: string
+    form: UseFormReturnType<ISkuFormValues>
+    handleSubmit: () => void
+    handleCancel: () => void
+    handleOrgSelect: (item: Organization) => void
+    handleSkuCatalogueSelect: (item: SkuCatalogue) => void
+}
+
+export default function SkuNewHTML(props: ISkuNewHTML) {
+    const [orgModalOpened, setOrgModalOpened] = useState(false)
+    const [skuCatModalOpened, setSkuCatModalOpened] = useState(false)
+
+    const navTrails: INavTrailProps[] = [
+        { title: "Dashboard", href: "/" },
+        { title: "Skus", href: "/inventory/skus" },
+        { title: "New", href: "#" },
+    ]
+
+    return (
+        <Page navTrails={navTrails}>
+            <PageHeader title={props.title!} />
+            <FormCard
+                submitButtonName="Create"
+                handleSubmit={props.form.onSubmit(props.handleSubmit)}
+                handleCancel={props.handleCancel}
+            >
+                {/* Select Contractor */}
+                {props.auther.isAdmin && props.orgUID == "" &&
+                    <Fragment>
+                        <OrgSelectModal
+                            opened={orgModalOpened}
+                            setOpened={setOrgModalOpened}
+                            handleSelect={props.handleOrgSelect}
+                        />
+                        <TextInput
+                            label="Organization"
+                            mb="md"
+                            placeholder="Select Organization"
+                            name="organization"
+                            onClick={() => setOrgModalOpened(true)}
+                            {...props.form.getInputProps("orgName")}
+                        />
+                    </Fragment>
+                }
+
+                {props.form.values.orgUID != "" &&
+                    <SkuCatalogueSelectModal
+                        opened={skuCatModalOpened}
+                        setOpened={setSkuCatModalOpened}
+                        handleSelect={props.handleSkuCatalogueSelect}
+                        organizationUID={props.form.values.orgUID}
+                    />
+                }
+                <TextInput
+                    label="Sku Catalogue"
+                    mb="md"
+                    placeholder="Select Sku Catalogue"
+                    disabled={props.form.values.orgUID != "" ? false : true}
+                    onClick={() => setSkuCatModalOpened(true)}
+                    {...props.form.getInputProps("skuName")}
+                />
+            </FormCard>
+        </Page>
+    )
+}
---
+++ src/modules/inventory/skus/SkuNew/index.tsx
@@ -0,0 +1,122 @@
+import { useEffect, useState } from "react"
+import * as Yup from "yup"
+import { useRouter } from "next/router"
+import { useForm, yupResolver } from "@mantine/form"
+import { useSkuCreateMutation, UpdateSku, SkuCatalogue, Organization, useAutherQuery } from "@lib/generated/hooks"
+import { showNotification } from "@mantine/notifications"
+import { getOrgFromLocalStorage } from "common/readLocalStorage"
+import PageLoader from "components/PageLoader"
+import { PageProps } from "types/types"
+import SkuNewHTML from "./SkuNewHTML"
+
+const schema = Yup.object().shape({
+    name: Yup.string().min(2, "Organization Name should have at least 2 letters"),
+    orgUID: Yup.string().min(2, "Invalid org UID"),
+})
+
+export default function SkuNew(props: PageProps) {
+    const router = useRouter()
+    const [newSku] = useSkuCreateMutation({})
+    const [autherLoaded, setAutherLoaded] = useState(false)
+    const [orgUID, setOrgUID] = useState("")
+
+    const form = useForm({
+        validate: yupResolver(schema),
+        initialValues: {
+            skuUID: "",
+            orgUID: "",
+
+            orgName: "",
+            skuName: "",
+        },
+    })
+
+    // get org uid from local storage
+    useEffect(() => {
+        const orgObj = getOrgFromLocalStorage()
+        setOrgUID(orgObj.uid)
+        if (orgObj.uid != "" && orgObj.name) {
+            form.values.orgUID = orgObj.uid!
+            form.values.orgName = orgObj.name!
+        }
+    }, [orgUID, form])
+
+    // load auther
+    const authData = useAutherQuery()
+    if (authData.loading) {
+        return (
+            <PageLoader />
+        )
+    }
+    if (authData.error) {
+        showNotification({
+            disallowClose: false,
+            color: "red",
+            message: authData.error.message,
+        })
+        return <PageLoader isError={true} />
+    }
+    if (authData.data && !autherLoaded) {
+        if (!authData.data.auther.isAdmin) {
+            form.setValues({ orgUID: authData.data.auther.orgUID })
+        }
+        setAutherLoaded(true)
+    }
+
+    const handleOrgSelect = (item: Organization) => {
+        if (item) {
+            form.values.orgUID = item.uid!
+            form.values.orgName = item.name!
+        }
+    }
+
+    const handleSkuCatalogueSelect = (item: SkuCatalogue | undefined) => {
+        if (item) {
+            form.values.skuUID = item?.uid!
+            form.values.skuName = item?.name!
+        }
+    }
+
+    const handleSubmit = () => {
+        var newSkuInput: UpdateSku = {
+            uid: form.values.skuUID,
+            orgUID: form.values.orgUID,
+        }
+
+        newSku({
+            variables: {input: newSkuInput}
+        }).then((res: any) => {
+            let welcomeMsg: string = `Sku ${res.data.skuCreate.name} Created`
+            
+            showNotification({
+                disallowClose: false,
+                color: "green",
+                message: welcomeMsg,
+            })
+            router.push("/inventory/skus")
+        }).catch((error: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "red",
+                message: error.message,
+            })
+        })
+    }
+
+    const handleCancel = () => {
+        router.push("/inventory/skus")
+    }
+
+    return (
+        <SkuNewHTML
+            title={props.title!}
+            auther={authData.data?.auther!}
+            orgUID={orgUID}
+            form={form}
+            handleSubmit={handleSubmit}
+            handleCancel={handleCancel}
+            handleOrgSelect={handleOrgSelect}
+            handleSkuCatalogueSelect={handleSkuCatalogueSelect}
+        />
+    )
+}
