9fa4f64c73f3e6ba1511265894461a2d9cb267c1
Tiger Darbey <tiger@pop-os.localdomain>
2023-03-12 09:28:45 +0530

    seperate out html components for sku list

---
+++ src/modules/inventory/skus/SkuList/SkuTable.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react"
+import { Sku, SkusResult } from "@lib/generated/hooks"
+import { Badge, Box, Text, useMantineTheme } from "@mantine/core"
+import { DataTable } from "mantine-datatable"
+import ContentCard from "components/ContentCard"
+import TableActionBar from "components/TableWrapper/TableActionBar"
+import TableRowActions from "components/TableWrapper/TableRowActions"
+import { PAGE_SIZES } from "types/enums"
+
+interface SkuTableProps {
+    data: SkusResult
+    viewAction: (item: Sku) => void
+    editAction: (item: Sku) => void
+    archiveAction: (item: Sku) => void
+    unarchiveAction: (item: Sku) => void
+    batchViewAction?: (selectedRecords: Sku[]) => void
+    filterAction?: (filter: string) => void
+    filterOptions: string[]
+}
+
+export default function SkuTable(props: SkuTableProps) {
+    const theme = useMantineTheme()
+
+    const [pageSize, setPageSize] = useState(PAGE_SIZES[1])
+    const [page, setPage] = useState(1)
+    const [records, setRecords] = useState<Sku[]>(props.data.skus.slice(0, pageSize))
+    const [selectedRecords, setSelectedRecords] = useState<Sku[]>([])
+
+    useEffect(() => {
+        const from = (page - 1) * pageSize
+        const to = from + pageSize
+        setRecords(props.data.skus.slice(from, to))
+    }, [page, pageSize, props.data.skus])
+
+    return (
+        <ContentCard>
+            <TableActionBar
+                selectedRecords={selectedRecords}
+                showActionButton={true}
+                batchViewAction={props.batchViewAction}
+                filterAction={props.filterAction}
+                filterOptions={props.filterOptions}
+            />
+            <Box>
+                <DataTable
+                    minHeight={300}
+                    verticalSpacing="md"
+                    horizontalSpacing="sm"
+                    highlightOnHover
+                    noRecordsText="No records to show"
+                    records={records}
+                    columns={[
+                        { accessor: "code", width: "10%" },
+                        { accessor: "name" },
+                        { accessor: "organization.name", title: "Organization" },
+                        { accessor: "batchCount", title: "Batch Count" },
+                        { accessor: "cartonCount", title: "Cartons Count" },
+                        {
+                            accessor: "status",
+                            // width: 160,
+                            render: (item) => (
+                                <Badge
+                                    color={!item.isArchived ? "blue" : "gray"}
+                                    variant={theme.colorScheme === "dark" ? "light" : "light"}
+                                    >
+                                    {!item.isArchived ? "Active" : "Disabled"}
+                                </Badge>
+                            )
+                        },
+                        {
+                            accessor: "actions",
+                            title: <Text mr="xs">Actions</Text>,
+                            textAlignment: "center",
+                            render: (item) => (
+                                <TableRowActions
+                                    item={item}
+                                    viewAction={props.viewAction}
+                                    editAction={props.editAction}
+                                    archiveAction={props.archiveAction}
+                                    unarchiveAction={props.unarchiveAction}
+                                />
+                            )
+                        },
+                    ]}
+                    selectedRecords={selectedRecords}
+                    onSelectedRecordsChange={setSelectedRecords}
+                    totalRecords={props.data.total}
+                    page={page}
+                    onPageChange={(p) => setPage(p)}
+                    recordsPerPage={pageSize}
+                    recordsPerPageOptions={PAGE_SIZES}
+                    onRecordsPerPageChange={setPageSize}
+                    recordsPerPageLabel="Rows Per Page"
+                />
+            </Box>
+        </ContentCard>
+    )
+}
