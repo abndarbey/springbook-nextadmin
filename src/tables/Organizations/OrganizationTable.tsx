85041375d8cb95c9045fd3bfe947ec5838a1a516
Tiger Darbey <tiger@pop-os.localdomain>
2023-03-28 13:43:38 +0530

    add org table

---
+++ src/tables/Organizations/OrganizationTable.tsx
@@ -0,0 +1,123 @@
+import { useEffect, useState } from "react"
+import { Organization, OrganizationsResult } from "@lib/generated/hooks"
+import { Badge, Box, Text, useMantineTheme } from "@mantine/core"
+import { DataTable } from "mantine-datatable"
+import ContentCard from "components/ContentCard"
+import TableActionBar from "components/TableWrapper/TableActionBar"
+import TableRowActions from "components/TableWrapper/TableRowActions"
+import { PAGE_SIZES } from "types/enums"
+
+interface OrganizationTableProps {
+    data: OrganizationsResult
+    viewAction: any
+    editAction: any
+    archiveAction: any
+    unarchiveAction: any
+    batchViewAction?: any
+    filterAction?: any
+    filterOptions: string[]
+}
+
+const sectorColors: Record<string, string> = {
+    manufacturing: 'blue',
+    trading: 'cyan',
+    tpl: 'pink',
+}
+
+export default function OrganizationsTable(props: OrganizationTableProps) {
+    const theme = useMantineTheme()
+
+    const [pageSize, setPageSize] = useState(PAGE_SIZES[1])
+    const [page, setPage] = useState(1)
+    const [records, setRecords] = useState<Organization[]>(props.data.organizations.slice(0, pageSize))
+    const [selectedRecords, setSelectedRecords] = useState<Organization[]>([])
+
+    useEffect(() => {
+        const from = (page - 1) * pageSize
+        const to = from + pageSize
+        setRecords(props.data.organizations.slice(from, to))
+    }, [page, pageSize, props.data.organizations])
+
+    return (
+        <ContentCard>
+            <TableActionBar
+                selectedRecords={selectedRecords}
+                showActionButton={true}
+                batchViewAction={props.batchViewAction}
+                filterAction={props.filterAction}
+                filterOptions={props.filterOptions}
+            />
+            <Box>
+                <DataTable
+                    minHeight={300}
+                    verticalSpacing="md"
+                    horizontalSpacing="sm"
+                    highlightOnHover
+                    noRecordsText="No records to show"
+                    records={records}
+                    columns={[
+                        { accessor: 'code', width: '10%' },
+                        { accessor: 'name', width: '20%' },
+                        { accessor: 'website', width: '20%' },
+                        // {
+                        //     accessor: 'website',
+                        //     width: '20%',
+                        //     render: (item) => (
+                        //         <Box>
+                        //             <p>{item.name}</p>
+                        //             <a href={item.website}>{item.website}</a>
+                        //         </Box>
+                        //     )
+                        // },
+                        {
+                            accessor: 'sector',
+                            render: (item) => (
+                                <Badge
+                                    color={sectorColors[item.sector!]}
+                                    variant={theme.colorScheme === 'dark' ? 'light' : 'outline'}
+                                    >
+                                    {item.sector}
+                                </Badge>
+                            )
+                        },
+                        {
+                            accessor: 'status',
+                            // width: 160,
+                            render: (item) => (
+                                <Badge
+                                    color={!item.isArchived ? 'blue' : 'gray'}
+                                    variant={theme.colorScheme === 'dark' ? 'light' : 'light'}
+                                    >
+                                    {!item.isArchived ? 'Active' : 'Disabled'}
+                                </Badge>
+                            )
+                        },
+                        {
+                            accessor: 'actions',
+                            title: <Text mr="xs">Actions</Text>,
+                            textAlignment: 'center',
+                            render: (item) => (
+                                <TableRowActions
+                                    item={item}
+                                    viewAction={props.viewAction}
+                                    editAction={props.editAction}
+                                    archiveAction={props.archiveAction}
+                                    unarchiveAction={props.unarchiveAction}
+                                />
+                            )
+                        },
+                    ]}
+                    selectedRecords={selectedRecords}
+                    onSelectedRecordsChange={setSelectedRecords}
+                    totalRecords={props.data.total}
+                    page={page}
+                    onPageChange={(p) => setPage(p)}
+                    recordsPerPage={pageSize}
+                    recordsPerPageOptions={PAGE_SIZES}
+                    onRecordsPerPageChange={setPageSize}
+                    recordsPerPageLabel='Rows Per Page'
+                />
+            </Box>
+        </ContentCard>
+    )
+}
---
+++ src/tables/Organizations/index.tsx
@@ -0,0 +1,136 @@
+import { useState } from "react"
+
+import {
+    useOrganizationsQuery,
+    useOrganizationArchiveMutation,
+    useOrganizationUnarchiveMutation,
+    Organization,
+    SortByOption,
+    SortDir,
+    FilterOption
+} from "@lib/generated/hooks"
+import PageLoader from "components/PageLoader"
+import { showNotification } from "@mantine/notifications"
+import { useRouter } from "next/router"
+import OrganizationsTable from "./OrganizationTable"
+
+export default function OrganizationList() {
+    const router = useRouter()
+    const [filterValue, setFilterValue] = useState<FilterOption>(FilterOption.All)
+    const [archiveRequest] = useOrganizationArchiveMutation({})
+    const [unarchiveRequest] = useOrganizationUnarchiveMutation({})
+
+    const filterOptions: string[] = ["All", "Active", "Archived"]
+
+    // fetch data
+    const { data, loading, error } = useOrganizationsQuery(
+        {
+            variables: {
+                searchFilter: {
+                    sortBy: SortByOption.DateCreated,
+                    sortDir: SortDir.Ascending,
+                    filter: filterValue,
+                    limit: 100,
+                    offset: 0,
+                },
+            }
+        }
+    )
+    if (loading) {
+        return (
+            <PageLoader />
+        )
+    }
+    if (!loading && error) {
+        showNotification({
+            disallowClose: false,
+            color: "red",
+            message: error.message,
+        })
+        return <PageLoader isError={true} />
+    }
+
+    const handleNew = () => {
+        router.push("/company/organizations/new")
+    }
+
+    // Row Actions
+    const viewAction = (item: Organization) => {
+        router.push(`/company/organizations/${item.code}`)
+    }
+    const editAction = (item: Organization) => {
+        router.push(`/company/organizations/${item.code}/edit`)
+    }
+
+    const archiveAction = (item: Organization) => {
+        archiveRequest({
+            variables: {uid: item.uid}
+        }).then((res: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "green",
+                message: `Archived - ${res.data.organizationArchive.name}`,
+            })
+        }).catch((error: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "red",
+                message: error.message,
+            })
+        })
+    }
+
+    const unarchiveAction = (item: Organization) => {
+        unarchiveRequest({
+            variables: {uid: item.uid}
+        }).then((res: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "green",
+                message: `Unarchived - ${res.data.organizationUnarchive.name}`,
+            })
+        }).catch((error: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "red",
+                message: error.message,
+            })
+        })
+    }
+
+    // Batch Actions
+    const batchViewAction = (selectedRecords: Organization[]) => {
+        selectedRecords.map((item, key) => {
+            console.log(item.code)
+        })
+    }
+
+    // Filter Actions
+    const filterAction = (filter: string) => {
+        if (filter === FilterOption.All) {
+            setFilterValue(FilterOption.All)
+        }
+        if (filter === FilterOption.Active) {
+            setFilterValue(FilterOption.Active)
+        }
+        if (filter === FilterOption.Draft) {
+            setFilterValue(FilterOption.Draft)
+        }
+        if (filter === FilterOption.Archived) {
+            setFilterValue(FilterOption.Archived)
+        }
+    }
+
+    return (
+        <OrganizationsTable
+            data={data?.organizations!}
+            viewAction={viewAction}
+            editAction={editAction}
+            archiveAction={archiveAction}
+            unarchiveAction={unarchiveAction}
+            batchViewAction={batchViewAction}
+            filterAction={filterAction}
+            filterOptions={filterOptions}
+        />
+    )
+}
