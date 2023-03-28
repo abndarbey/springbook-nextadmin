7e8e3c50d91536e77f00d8ea9850ff2d041821f2
Tiger Darbey <tiger@pop-os.localdomain>
2023-03-28 13:46:17 +0530

    add dept table

---
+++ src/tables/Departments/index.tsx
@@ -0,0 +1,137 @@
+import { useState } from "react"
+import {
+    useDepartmentsQuery,
+    useDepartmentArchiveMutation,
+    useDepartmentUnarchiveMutation,
+    Department,
+    SortByOption,
+    SortDir,
+    FilterOption
+} from "@lib/generated/hooks"
+import PageLoader from "components/PageLoader"
+import { showNotification } from "@mantine/notifications"
+import { useRouter } from "next/router"
+import DepartmentTable from "./DepartmentTable"
+
+interface DepartmentsProps {
+    orgUID?: string | null | undefined
+}
+
+export default function Departments(props: DepartmentsProps) {
+    const router = useRouter()
+
+    const [filterValue, setFilterValue] = useState<FilterOption>(FilterOption.All)
+    const [archiveRequest] = useDepartmentArchiveMutation({})
+    const [unarchiveRequest] = useDepartmentUnarchiveMutation({})
+
+    const filterOptions: string[] = ["All", "Active", "Archived"]
+
+    // fetch data
+    const { data, loading, error } = useDepartmentsQuery(
+        {
+            variables: {
+                searchFilter: {
+                    sortBy: SortByOption.DateCreated,
+                    sortDir: SortDir.Ascending,
+                    filter: filterValue,
+                    orgUID: props.orgUID,
+                    limit: 100,
+                    offset: 0,
+                },
+            }
+        }
+    )
+    if (loading) {
+        return (
+            <PageLoader />
+        )
+    }
+    if (!loading && error) {
+        showNotification({
+            disallowClose: false,
+            color: "red",
+            message: error.message,
+        })
+        return <PageLoader isError={true} />
+    }
+
+    // Row Actions
+    const viewAction = (item: Department) => {
+        router.push(`/company/departments/${item.code}`)
+    }
+    const editAction = (item: Department) => {
+        router.push(`/company/departments/${item.code}/edit`)
+    }
+
+    const archiveAction = (item: Department) => {
+        archiveRequest({
+            variables: {id: item.id!}
+        }).then((res: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "green",
+                message: `Archived - ${res.data.organizationArchive.name}`,
+            })
+        }).catch((error: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "red",
+                message: error.message,
+            })
+        })
+    }
+
+    const unarchiveAction = (item: Department) => {
+        unarchiveRequest({
+            variables: {id: item.id!}
+        }).then((res: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "green",
+                message: `Unarchived - ${res.data.organizationUnarchive.name}`,
+            })
+        }).catch((error: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "red",
+                message: error.message,
+            })
+        })
+    }
+
+    // Batch Actions
+    const batchViewAction = (selectedRecords: Department[]) => {
+        selectedRecords.map((item, key) => {
+            console.log(item.code)
+        })
+    }
+
+    // Filter Actions
+    const filterAction = (filter: string) => {
+        if (filter === FilterOption.All) {
+            setFilterValue(FilterOption.All)
+        }
+        if (filter === FilterOption.Active) {
+            setFilterValue(FilterOption.Active)
+        }
+        if (filter === FilterOption.Draft) {
+            setFilterValue(FilterOption.Draft)
+        }
+        if (filter === FilterOption.Archived) {
+            setFilterValue(FilterOption.Archived)
+        }
+    }
+
+    return (
+        <DepartmentTable
+            data={data?.departments!}
+            viewAction={viewAction}
+            editAction={editAction}
+            archiveAction={archiveAction}
+            unarchiveAction={unarchiveAction}
+            batchViewAction={batchViewAction}
+            filterAction={filterAction}
+            filterOptions={filterOptions}
+        />
+    )
+}
