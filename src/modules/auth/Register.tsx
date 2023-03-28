a6ee720c199e8ed017562aa0ecb0c02285864a95
Tiger Darbey <tiger@pop-os.localdomain>
2023-03-28 13:42:41 +0530

    add sectore to org register

--- src/modules/auth/Register.tsx
+++ src/modules/auth/Register.tsx
@@ -1,25 +1,26 @@
-import { Fragment } from 'react'
-import { useRouter } from 'next/router'
-import * as Yup from 'yup'
-import { useForm, yupResolver } from '@mantine/form'
-import { showNotification } from '@mantine/notifications'
+import { Fragment } from "react"
+import { useRouter } from "next/router"
+import * as Yup from "yup"
+import { useForm, yupResolver } from "@mantine/form"
+import { showNotification } from "@mantine/notifications"
 import {
     TextInput,
     Paper,
     Container,
     Group,
     Button,
-} from '@mantine/core'
-// import { IconUpload } from '@tabler/icons'
-import { AuthCardTitle } from './authElements'
-import { useOrganizationRegisterMutation, RegisterOrganization } from '@lib/generated/hooks'
+    SimpleGrid,
+} from "@mantine/core"
+// import { IconUpload } from "@tabler/icons"
+import { AuthCardTitle } from "./authElements"
+import { useOrganizationRegisterMutation, RegisterOrganization } from "@lib/generated/hooks"
 
 const schema = Yup.object().shape({
-    orgName: Yup.string().min(2, 'Organization Name should have at least 2 letters'),
-    firstName: Yup.string().min(2, 'First Name should have at least 2 letters'),
-    lastName: Yup.string().min(2, 'Last Name should have at least 2 letters'),
-    email: Yup.string().email('Invalid email'),
-    phone: Yup.string().min(10, 'Phone should have at least 10 numbers'),
+    orgName: Yup.string().min(2, "Organization Name should have at least 2 letters"),
+    firstName: Yup.string().min(2, "First Name should have at least 2 letters"),
+    lastName: Yup.string().min(2, "Last Name should have at least 2 letters"),
+    email: Yup.string().email("Invalid email"),
+    phone: Yup.string().min(10, "Phone should have at least 10 numbers"),
 })
 
 const Register = () => {
@@ -29,14 +30,15 @@ const Register = () => {
     const form = useForm({
         validate: yupResolver(schema),
         initialValues: {
-            orgName: '',
-            website: '',
-            // logo: '',
-            // banner: '',
-            firstName: '',
-            lastName: '',
-            email: '',
-            phone: '',
+            orgName: "",
+            website: "",
+            sector: "",
+            // logo: "",
+            // banner: "",
+            firstName: "",
+            lastName: "",
+            email: "",
+            phone: "",
         },
     })
 
@@ -44,6 +46,7 @@ const Register = () => {
         var registerInput: RegisterOrganization = {
             orgName: form.values.orgName,
             website: form.values.website,
+            sector: form.values.sector,
             firstName: form.values.firstName,
             lastName: form.values.lastName,
             email: form.values.email,
@@ -57,14 +60,14 @@ const Register = () => {
             
             showNotification({
                 disallowClose: false,
-                color: 'green',
+                color: "green",
                 message: welcomeMsg,
             })
-            router.push('/login')
+            router.push("/login")
         }).catch((error: any) => {
             showNotification({
                 disallowClose: false,
-                color: 'red',
+                color: "red",
                 message: error.message,
             })
         })
@@ -73,35 +76,43 @@ const Register = () => {
     return (
         <Fragment>
             <Container size={600} my={40}>
-                <AuthCardTitle pageType='register' />
+                <AuthCardTitle pageType="register" />
                 <Paper withBorder shadow="md" p={30} mt={30} radius="md">
                     <form onSubmit={form.onSubmit(handleSubmit)}>
                         <TextInput
                             label="Organization Name"
                             placeholder="Example Company Pvt Ltd"
                             required
-                            {...form.getInputProps('orgName')}
+                            {...form.getInputProps("orgName")}
                         />
-                        <TextInput
+                        <SimpleGrid cols={2}>
+                            <TextInput
+                                mt="md"
+                                label="Website"
+                                placeholder="www.example.com"
+                                {...form.getInputProps("website")}
+                            />
+                            <TextInput
                             mt="md"
-                            label="Website"
-                            placeholder="www.example.com"
-                            {...form.getInputProps('website')}
+                            label="Sector"
+                            placeholder="Trader"
+                            {...form.getInputProps("sector")}
                         />
+                        </SimpleGrid>
                         {/* <Group grow mt="md">
                             <FileInput
                                 label="Logo"
                                 placeholder="Upload Logo"
                                 accept="image/png,image/jpeg"
                                 icon={<IconUpload size={14}/>}
-                                {...form.getInputProps('logo')}
+                                {...form.getInputProps("logo")}
                             />
                             <FileInput
                                 label="Banner"
                                 placeholder="Upload Banner Image"
                                 accept="image/png,image/jpeg"
                                 icon={<IconUpload size={14}/>}
-                                {...form.getInputProps('logo')}
+                                {...form.getInputProps("logo")}
                             />
                         </Group> */}
                         <Group grow mt="md">
@@ -109,13 +120,13 @@ const Register = () => {
                                 label="First Name"
                                 placeholder="Your Name"
                                 required
-                                {...form.getInputProps('firstName')}
+                                {...form.getInputProps("firstName")}
                             />
                             <TextInput
                                 label="Last Name"
                                 placeholder="Your Surname"
                                 required
-                                {...form.getInputProps('lastName')}
+                                {...form.getInputProps("lastName")}
                             />
                         </Group>
                         <Group grow mt="md">
@@ -123,13 +134,13 @@ const Register = () => {
                                 label="Email"
                                 placeholder="yourname@example.com"
                                 required
-                                {...form.getInputProps('email')}
+                                {...form.getInputProps("email")}
                             />
                             <TextInput
                                 label="Phone"
                                 placeholder="1234567890"
                                 required
-                                {...form.getInputProps('phone')}
+                                {...form.getInputProps("phone")}
                             />
                         </Group>
                         <Button type="submit" fullWidth mt="xl">
