87a3e8d7cda9f310d46cd7ab3edaeb19d53288a6
Tiger Darbey <tiger@pop-os.localdomain>
2023-03-28 13:45:54 +0530

    modify org details page

---
+++ src/modules/company/organizations/OrganizationDetails/OrganizationPage.tsx
@@ -0,0 +1,27 @@
+import { Organization } from '@lib/generated/hooks'
+import { Box, SimpleGrid } from '@mantine/core'
+import ContentCard from 'components/ContentCard'
+import DetailRow from 'components/DetailRow'
+import React from 'react'
+
+interface OrganizationPageProps {
+    data: Organization
+}
+
+export default function OrganizationPage(props: OrganizationPageProps) {
+    return (
+        <ContentCard>
+            <SimpleGrid cols={2} breakpoints={[{ maxWidth: 755, cols: 1 }]}>
+                <Box sx={(theme) => ({borderRadius: theme.radius.md})}>
+                    <DetailRow title="Code" value={props.data.code!} />
+                    <DetailRow title="Name" value={props.data.name!} />
+                    <DetailRow title="Website" value={props.data.website!} />
+                </Box>
+                <Box sx={(theme) => ({borderRadius: theme.radius.md})}>
+                    <DetailRow title="Sector" value={props.data?.sector!} />
+                    <DetailRow title="Status" value={props.data?.status!} />
+                </Box>
+            </SimpleGrid>
+        </ContentCard>
+    )
+}
---
+++ src/modules/company/organizations/OrganizationDetails/index.tsx
@@ -0,0 +1,124 @@
+import { useRouter } from "next/router"
+import { Tabs } from "@mantine/core"
+
+import Page from "components/Page"
+import PageHeader from "components/PageHeader"
+import { INavTrailProps } from "components/NavTrails"
+import { IActionButtonProps } from "components/PageHeader/ActionButtons"
+import {
+    useOrganizationQuery,
+    useOrganizationArchiveMutation,
+    useOrganizationUnarchiveMutation,
+} from "@lib/generated/hooks"
+import PageLoader from "components/PageLoader"
+import { showNotification } from "@mantine/notifications"
+import { PageProps } from "types/types"
+import OrganizationPage from "./OrganizationPage"
+import Departments from "tables/Departments"
+
+export default function OrganizationDetails(props: PageProps) {
+    const router = useRouter()
+    const [archiveRequest] = useOrganizationArchiveMutation({})
+    const [unarchiveRequest] = useOrganizationUnarchiveMutation({})
+
+    const navTrails: INavTrailProps[] = [
+        { title: "Dashboard", href: "/" },
+        { title: "Organizations", href: "/company/organizations" },
+        { title: props.code, href: "#" },
+    ]
+
+    // fetch data
+    const { data, loading, error } = useOrganizationQuery(
+        {
+            variables: {
+                code: props.code,
+            }
+        }
+    )
+    if (loading) {
+        return (
+            <PageLoader />
+        )
+    }
+    if (!loading && error) {
+        showNotification({
+            disallowClose: false,
+            color: "red",
+            message: error.message,
+        })
+        return <PageLoader isError={true} />
+    }
+
+    // edit action
+    const handleEdit = (e: any) => {
+        e.preventDefault()
+        router.push(`/company/organizations/${props.code}/edit`)
+    }
+    
+    // archive action
+    const handleArchive = (e: any) => {
+        e.preventDefault()
+        archiveRequest({
+            variables: {uid: data?.organization.uid!}
+        }).then((res: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "green",
+                message: `Archived - ${res.data.organizationArchive.name}`,
+            })
+        }).catch((error: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "red",
+                message: error.message,
+            })
+        })
+    }
+    
+    // unarchive action
+    const handleUnarchive = (e: any) => {
+        e.preventDefault()
+        unarchiveRequest({
+            variables: {uid: data?.organization.uid!}
+        }).then((res: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "green",
+                message: `Unarchived - ${res.data.organizationUnarchive.name}`,
+            })
+        }).catch((error: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "red",
+                message: error.message,
+            })
+        })
+    }
+
+    // define action buttons
+    const actionButtons: IActionButtonProps[] = [
+        { type: "edit", name: "Edit", action: handleEdit },
+        { type: "archive", name: "Archive", action: handleArchive, disabled: data?.organization.isArchived! },
+        { type: "unarchive", name: "Unarchive", action: handleUnarchive, disabled: !data?.organization.isArchived! },
+    ]
+
+    return (
+        <Page navTrails={navTrails}>
+            <PageHeader title={props.title!} buttons={actionButtons} />
+            <Tabs variant="pills" radius="xs" defaultValue="details">
+                <Tabs.List>
+                    <Tabs.Tab value="details">Details</Tabs.Tab>
+                    <Tabs.Tab value="departments">Departments</Tabs.Tab>
+                </Tabs.List>
+
+                <Tabs.Panel value="details" pt="xs">
+                    <OrganizationPage data={data?.organization!} />
+                </Tabs.Panel>
+
+                <Tabs.Panel value="departments" pt="xs">
+                    <Departments orgUID={data?.organization.uid} />
+                </Tabs.Panel>
+            </Tabs>
+        </Page>
+    )
+}
