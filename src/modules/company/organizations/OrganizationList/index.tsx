737b97d38392ba1c82ee9b609b1cbc0aee6cff71
Tiger Darbey <tiger@pop-os.localdomain>
2023-03-28 13:44:32 +0530

    modify org list page

---
+++ src/modules/company/organizations/OrganizationList/index.tsx
@@ -0,0 +1,36 @@
+import { INavTrailProps } from "components/NavTrails"
+import Page from "components/Page"
+import PageHeader from "components/PageHeader"
+import { useRouter } from "next/router"
+import { IActionButtonProps } from "components/PageHeader/ActionButtons"
+import { PageProps } from "types/types"
+import Organizations from "tables/Organizations"
+
+const navTrails: INavTrailProps[] = [
+    { title: "Dashboard", href: "/" },
+    { title: "Organizations", href: "#" },
+]
+
+export default function DepartmentList(props: PageProps) {
+    const router = useRouter()
+
+    const handleNew = () => {
+        router.push("/company/organizations/new")
+    }
+
+    const actionButtons: IActionButtonProps[] = [
+        {
+            type: "new",
+            name: "New",
+            disabled: false,
+            action: handleNew,
+        }
+    ]
+
+    return (
+        <Page navTrails={navTrails}>
+            <PageHeader title={props.title!} buttons={actionButtons} />
+            <Organizations />
+        </Page>
+    )
+}
