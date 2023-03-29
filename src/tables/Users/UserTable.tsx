6fa55705954ff5c4b1786ed8163627b0bf9e4a11
Tiger Darbey <tiger@pop-os.localdomain>
2023-03-29 16:23:39 +0530

    add user table component

---
+++ src/tables/Users/UserTable.tsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from "react"
+import { Badge, Box, Text, useMantineTheme } from "@mantine/core"
+import { User, UserResult } from "@lib/generated/hooks"
+import { PAGE_SIZES } from "types/enums"
+import ContentCard from "components/ContentCard"
+import TableActionBar from "components/TableWrapper/TableActionBar"
+import { DataTable } from "mantine-datatable"
+import TableRowActions from "components/TableWrapper/TableRowActions"
+
+interface UserTableProps {
+    data: UserResult
+    viewAction: any
+    editAction: any
+    archiveAction: any
+    unarchiveAction: any
+    batchViewAction?: any
+    filterAction?: any
+    filterOptions: string[]
+}
+
+export default function UserTable(props: UserTableProps) {
+    const theme = useMantineTheme()
+
+    const [pageSize, setPageSize] = useState(PAGE_SIZES[1])
+    const [page, setPage] = useState(1)
+    const [records, setRecords] = useState<User[]>(props.data.users.slice(0, pageSize))
+    const [selectedRecords, setSelectedRecords] = useState<User[]>([])
+
+    useEffect(() => {
+        const from = (page - 1) * pageSize
+        const to = from + pageSize
+        setRecords(props.data.users.slice(from, to))
+    }, [page, pageSize, props.data.users])
+
+    return (
+        <ContentCard>
+            <TableActionBar
+                selectedRecords={selectedRecords}
+                showActionButton={true}
+                batchViewAction={props.batchViewAction}
+                filterAction={props.filterAction}
+                filterOptions={props.filterOptions}
+            />
+            <Box>
+                <DataTable
+                    minHeight={300}
+                    verticalSpacing="md"
+                    horizontalSpacing="sm"
+                    highlightOnHover
+                    noRecordsText="No records to show"
+                    records={records}
+                    columns={[
+                        {
+                            accessor: 'name',
+                            render: (item) => (<>{item.firstName} {item.lastName}</>)
+                        },
+                        { accessor: 'email' },
+                        {
+                            accessor: 'organization',
+                            render: (item) => (<>{item.organization?.code ? item.organization?.code : "NA"}</>)
+                        },
+                        {
+                            accessor: 'status',
+                            // width: 160,
+                            render: (item) => (
+                                <Badge
+                                    color={!item.isArchived ? 'blue' : 'gray'}
+                                    variant={theme.colorScheme === 'dark' ? 'light' : 'light'}
+                                    >
+                                    {!item.isArchived ? 'Active' : 'Disabled'}
+                                </Badge>
+                            )
+                        },
+                        {
+                            accessor: 'actions',
+                            title: <Text mr="xs">Actions</Text>,
+                            textAlignment: 'center',
+                            render: (item) => (
+                                <TableRowActions
+                                    item={item}
+                                    viewAction={props.viewAction}
+                                    editAction={props.editAction}
+                                    archiveAction={props.archiveAction}
+                                    unarchiveAction={props.unarchiveAction}
+                                />
+                            )
+                        },
+                    ]}
+                    selectedRecords={selectedRecords}
+                    onSelectedRecordsChange={setSelectedRecords}
+                    totalRecords={props.data.total}
+                    page={page}
+                    onPageChange={(p) => setPage(p)}
+                    recordsPerPage={pageSize}
+                    recordsPerPageOptions={PAGE_SIZES}
+                    onRecordsPerPageChange={setPageSize}
+                    recordsPerPageLabel='Rows Per Page'
+                />
+            </Box>
+        </ContentCard>
+    )
+}
---
+++ src/tables/Users/index.tsx
@@ -0,0 +1,136 @@
+import { useState } from "react"
+import {
+    useUsersQuery,
+    useUserArchiveMutation,
+    useUserUnarchiveMutation,
+    User,
+    SortByOption,
+    SortDir,
+    FilterOption
+} from "@lib/generated/hooks"
+import PageLoader from "components/PageLoader"
+import { showNotification } from "@mantine/notifications"
+import { useRouter } from "next/router"
+import UserTable from "./UserTable"
+
+interface UserListProps {
+    roleID?: string | null | undefined
+}
+
+export default function Users(props: UserListProps) {
+    const router = useRouter()
+    const [filterValue, setFilterValue] = useState<FilterOption>(FilterOption.All)
+    const [archiveRequest] = useUserArchiveMutation({})
+    const [unarchiveRequest] = useUserUnarchiveMutation({})
+
+    const filterOptions: string[] = ["All", "Active", "Archived"]
+
+    // fetch data
+    const { data, loading, error } = useUsersQuery(
+        {
+            variables: {
+                searchFilter: {
+                    sortBy: SortByOption.DateCreated,
+                    sortDir: SortDir.Ascending,
+                    filter: filterValue,
+                    limit: 100,
+                    offset: 0,
+                },
+                roleID: props.roleID
+            }
+        }
+    )
+    if (loading) {
+        return (
+            <PageLoader />
+        )
+    }
+    if (!loading && error) {
+        showNotification({
+            disallowClose: false,
+            color: "red",
+            message: error.message,
+        })
+        return <PageLoader isError={true} />
+    }
+
+    // Row Actions
+    const viewAction = (item: User) => {
+        router.push(`/company/users/${item.email}`)
+    }
+    const editAction = (item: User) => {
+        router.push(`/company/users/${item.email}/edit`)
+    }
+
+    const archiveAction = (item: User) => {
+        archiveRequest({
+            variables: {id: item.id!}
+        }).then((res: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "green",
+                message: `Archived - ${res.data.userArchive.name}`,
+            })
+        }).catch((error: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "red",
+                message: error.message,
+            })
+        })
+    }
+
+    const unarchiveAction = (item: User) => {
+        unarchiveRequest({
+            variables: {id: item.id!}
+        }).then((res: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "green",
+                message: `Unarchived - ${res.data.userUnarchive.name}`,
+            })
+        }).catch((error: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "red",
+                message: error.message,
+            })
+        })
+    }
+
+    // Batch Actions
+    const batchViewAction = (selectedRecords: User[]) => {
+        selectedRecords.map((item, key) => {
+            console.log(item.email)
+        })
+    }
+
+    // Filter Actions
+    const filterAction = (filter: string) => {
+        if (filter === FilterOption.All) {
+            setFilterValue(FilterOption.All)
+        }
+        if (filter === FilterOption.Active) {
+            setFilterValue(FilterOption.Active)
+        }
+        if (filter === FilterOption.Draft) {
+            setFilterValue(FilterOption.Draft)
+        }
+        if (filter === FilterOption.Archived) {
+            setFilterValue(FilterOption.Archived)
+        }
+    }
+
+    return (
+        <UserTable
+            data={data?.users!}
+            viewAction={viewAction}
+            editAction={editAction}
+            archiveAction={archiveAction}
+            unarchiveAction={unarchiveAction}
+            batchViewAction={batchViewAction}
+            filterAction={filterAction}
+            filterOptions={filterOptions}
+        />
+    )
+}
