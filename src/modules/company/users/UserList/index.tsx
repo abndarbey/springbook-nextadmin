8172bc60f9fe53f8cd5b3fba3080ef1278e68c11
Tiger Darbey <tiger@pop-os.localdomain>
2023-03-29 16:27:08 +0530

    update user list page

---
+++ src/modules/company/users/UserList/index.tsx
@@ -0,0 +1,35 @@
+import { INavTrailProps } from 'components/NavTrails'
+import Page from 'components/Page'
+import PageHeader from 'components/PageHeader'
+import { useRouter } from 'next/router'
+import { IActionButtonProps } from 'components/PageHeader/ActionButtons'
+import Users from 'tables/Users'
+
+const navTrails: INavTrailProps[] = [
+    { title: 'Dashboard', href: '/' },
+    { title: 'Users', href: '#' },
+]
+
+export default function UserList() {
+    const router = useRouter()
+
+    const handleNew = () => {
+        router.push('/company/users/new')
+    }
+
+    const actionButtons: IActionButtonProps[] = [
+        {
+            type: 'new',
+            name: 'New',
+            disabled: false,
+            action: handleNew,
+        }
+    ]
+
+    return (
+        <Page navTrails={navTrails}>
+            <PageHeader title='Users' buttons={actionButtons} />
+            <Users />
+        </Page>
+    )
+}
