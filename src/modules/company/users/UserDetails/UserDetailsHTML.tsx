3d558a155bd15c4db65aed9e29276d821af3cc74
Tiger Darbey <tiger@pop-os.localdomain>
2023-03-30 08:10:11 +0530

    update user details page

---
+++ src/modules/company/users/UserDetails/UserDetailsHTML.tsx
@@ -0,0 +1,31 @@
+import { User } from "@lib/generated/hooks"
+import { Box, SimpleGrid } from "@mantine/core"
+import ContentCard from "components/ContentCard"
+import DetailRow from "components/DetailRow"
+import React from "react"
+
+interface UserPageProps {
+    data: User
+}
+
+export default function UserPage(props: UserPageProps) {
+    return (
+        <ContentCard>
+            <SimpleGrid cols={3} breakpoints={[{ maxWidth: 755, cols: 1 }]}>
+                <Box sx={(theme) => ({borderRadius: theme.radius.md})}>
+                    <DetailRow title="Name" value={props.data.firstName! + " " + props.data.lastName!} />
+                    <DetailRow title="Email" value={props.data.email!} />
+                    <DetailRow title="Phone" value={props.data.phone!} />
+                </Box>
+                <Box sx={(theme) => ({borderRadius: theme.radius.md})}>
+                    <DetailRow title="User Code" value={props.data?.role?.code!} />
+                    <DetailRow title="User Name" value={props.data?.role?.name!} />
+                </Box>
+                <Box sx={(theme) => ({borderRadius: theme.radius.md})}>
+                    <DetailRow title="Organization Code" value={props.data?.organization?.code!} />
+                    <DetailRow title="Organization Name" value={props.data?.organization?.name!} />
+                </Box>
+            </SimpleGrid>
+        </ContentCard>
+    )
+}
---
+++ src/modules/company/users/UserDetails/index.tsx
@@ -0,0 +1,110 @@
+import { useRouter } from "next/router"
+import { Tabs } from "@mantine/core"
+
+import Page from "components/Page"
+import PageHeader from "components/PageHeader"
+import { INavTrailProps } from "components/NavTrails"
+import { IActionButtonProps } from "components/PageHeader/ActionButtons"
+import {
+    useUserQuery,
+    useUserArchiveMutation,
+    useUserUnarchiveMutation,
+} from "@lib/generated/hooks"
+import PageLoader from "components/PageLoader"
+import { showNotification } from "@mantine/notifications"
+import { PageProps } from "types/types"
+import UserDetailsHTML from "./UserDetailsHTML"
+
+export default function UserDetails(props: PageProps) {
+    const router = useRouter()
+    const [archiveRequest] = useUserArchiveMutation({})
+    const [unarchiveRequest] = useUserUnarchiveMutation({})
+
+    const navTrails: INavTrailProps[] = [
+        { title: "Dashboard", href: "/" },
+        { title: "Users", href: "/company/users" },
+        { title: props.code, href: "#" },
+    ]
+
+    // fetch data
+    const { data, loading, error } = useUserQuery(
+        {
+            variables: {
+                email: props.code,
+            }
+        }
+    )
+    if (loading) {
+        return (
+            <PageLoader />
+        )
+    }
+    if (!loading && error) {
+        showNotification({
+            disallowClose: false,
+            color: "red",
+            message: error.message,
+        })
+        return <PageLoader isError={true} />
+    }
+
+    // edit action
+    const handleEdit = (e: any) => {
+        e.preventDefault()
+        router.push(`/company/users/${props.code}/edit`)
+    }
+    
+    // archive action
+    const handleArchive = (e: any) => {
+        e.preventDefault()
+        archiveRequest({
+            variables: {id: data?.user.id!}
+        }).then((res: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "green",
+                message: `Archived - ${res.data.userArchive.firstName}`,
+            })
+        }).catch((error: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "red",
+                message: error.message,
+            })
+        })
+    }
+    
+    // unarchive action
+    const handleUnarchive = (e: any) => {
+        e.preventDefault()
+        unarchiveRequest({
+            variables: {id: data?.user.id!}
+        }).then((res: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "green",
+                message: `Unarchived - ${res.data.userUnarchive.firstName}`,
+            })
+        }).catch((error: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "red",
+                message: error.message,
+            })
+        })
+    }
+
+    // define action buttons
+    const actionButtons: IActionButtonProps[] = [
+        { type: "edit", name: "Edit", action: handleEdit },
+        { type: "archive", name: "Archive", action: handleArchive, disabled: data?.user.isArchived! },
+        { type: "unarchive", name: "Unarchive", action: handleUnarchive, disabled: !data?.user.isArchived! },
+    ]
+
+    return (
+        <Page navTrails={navTrails}>
+            <PageHeader title={props.title!} buttons={actionButtons} />
+            <UserDetailsHTML data={data?.user!} />
+        </Page>
+    )
+}
