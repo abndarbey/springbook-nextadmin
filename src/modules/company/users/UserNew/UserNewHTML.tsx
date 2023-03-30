53923abaecb4a7250c12272ddfd0da3b3113a27e
Tiger Darbey <tiger@pop-os.localdomain>
2023-03-30 07:50:25 +0530

    update user new page

---
+++ src/modules/company/users/UserNew/UserNewHTML.tsx
@@ -0,0 +1,153 @@
+import { Fragment, useState } from "react"
+import Page from "components/Page"
+
+import PageHeader from "components/PageHeader"
+import FormCard from "components/FormCard"
+import OrgSelectModal from "common/select-table/OrgSelectModal"
+import { INavTrailProps } from "components/NavTrails"
+import { Auther, Department, Organization, Role } from "@lib/generated/hooks"
+import { UseFormReturnType } from "@mantine/form"
+import { SimpleGrid, TextInput } from "@mantine/core"
+import DepartmentSelectModal from "common/select-table/DepartmentSelectModal"
+import RoleSelectModal from "common/select-table/RoleSelectModal"
+
+interface IUserFormValues {
+    firstName: string,
+    lastName: string,
+    email: string,
+    phone: string,
+    orgUID: string,
+    roleID: string,
+    
+    departmentID: string,
+    orgName: string,
+    departmentName: string,
+    roleName: string,
+}
+
+interface IUserNewHTML {
+    title: string
+    auther: Auther
+    orgUID: string
+    form: UseFormReturnType<IUserFormValues>
+    handleSubmit: () => void
+    handleCancel: () => void
+    handleOrgSelect: (item: Organization) => void
+    handleDepartmentSelect: (item: Department) => void
+    handleRoleSelect: (item: Role) => void
+}
+
+export default function UserNewHTML(props: IUserNewHTML) {
+    const [orgModalOpened, setOrgModalOpened] = useState(false)
+    const [deptModalOpened, setDeptModalOpened] = useState(false)
+    const [roleModalOpened, setRoleModalOpened] = useState(false)
+
+    const navTrails: INavTrailProps[] = [
+        { title: "Dashboard", href: "/" },
+        { title: "Users", href: "/company/users" },
+        { title: "New", href: "#" },
+    ]
+
+    return (
+        <Page navTrails={navTrails}>
+            <PageHeader title="New User" />
+            <FormCard
+                submitButtonName="Create"
+                handleSubmit={props.form.onSubmit(props.handleSubmit)}
+                handleCancel={props.handleCancel}
+            >
+                <SimpleGrid cols={2} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
+                    <TextInput
+                        mb="md"
+                        label="First Name"
+                        placeholder="First name"
+                        name="firstName"
+                        {...props.form.getInputProps('firstName')}
+                    />
+                    <TextInput
+                        mb="md"
+                        label="Last Name"
+                        placeholder="Last name"
+                        name="lastName"
+                        {...props.form.getInputProps('lastName')}
+                    />
+                </SimpleGrid>
+                <SimpleGrid cols={2} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
+                    <TextInput
+                        label="Email"
+                        placeholder="Email"
+                        mb="md"
+                        name="email"
+                        {...props.form.getInputProps('email')}
+                    />
+                    <TextInput
+                        label="Phone"
+                        placeholder="Phone"
+                        mb="md"
+                        name="phone"
+                        {...props.form.getInputProps('phone')}
+                    />
+                </SimpleGrid>
+                {/* Select Organization */}
+                {props.auther.isAdmin && props.orgUID == "" &&
+                    <Fragment>
+                        <OrgSelectModal
+                            opened={orgModalOpened}
+                            setOpened={setOrgModalOpened}
+                            handleSelect={props.handleOrgSelect}
+                        />
+                        <TextInput
+                            label="Organization"
+                            mb="md"
+                            placeholder="Select Organization"
+                            name="organization"
+                            onClick={() => setOrgModalOpened(true)}
+                            {...props.form.getInputProps("orgName")}
+                        />
+                    </Fragment>
+                }
+                {props.form.values.orgUID != "" &&
+                    <DepartmentSelectModal
+                        opened={deptModalOpened}
+                        setOpened={setDeptModalOpened}
+                        handleSelect={props.handleDepartmentSelect}
+                        organizationUID={props.form.values.orgUID}
+                    />
+                }
+                {props.form.values.orgUID != "" &&
+                    <DepartmentSelectModal
+                        opened={deptModalOpened}
+                        setOpened={setDeptModalOpened}
+                        handleSelect={props.handleDepartmentSelect}
+                        organizationUID={props.form.values.orgUID}
+                    />
+                }
+                <TextInput
+                    label="Department"
+                    placeholder="Select Department"
+                    mb="md"
+                    disabled={props.form.values.orgUID != "" ? false : true}
+                    onClick={() => setDeptModalOpened(true)}
+                    {...props.form.getInputProps('departmentName')}
+                />
+
+                {props.form.values.departmentID != "" &&
+                    <RoleSelectModal
+                        opened={roleModalOpened}
+                        setOpened={setRoleModalOpened}
+                        handleSelect={props.handleRoleSelect}
+                        departmentID={props.form.values.departmentID}
+                    />
+                }
+                <TextInput
+                    label="Role"
+                    placeholder="Select Role"
+                    mb="md"
+                    disabled={props.form.values.departmentID != "" ? false : true}
+                    onClick={() => setRoleModalOpened(true)}
+                    {...props.form.getInputProps('roleName')}
+                />
+            </FormCard>
+        </Page>
+    )
+}
---
+++ src/modules/company/users/UserNew/index.tsx
@@ -0,0 +1,138 @@
+import { useEffect, useState } from "react"
+import * as Yup from "yup"
+import { useRouter } from "next/router"
+import { useForm, yupResolver } from "@mantine/form"
+import { useUserCreateMutation, UpdateUser, Organization, useAutherQuery, Department, Role } from "@lib/generated/hooks"
+import { showNotification } from "@mantine/notifications"
+import { getObjectFromLocalStorage } from "common/localStorage"
+import PageLoader from "components/PageLoader"
+import { PageProps } from "types/types"
+import UserNewHTML from "./UserNewHTML"
+
+const schema = Yup.object().shape({
+    name: Yup.string().min(2, "Organization Name should have at least 2 letters"),
+    orgUID: Yup.string().min(2, "Invalid org UID"),
+})
+
+export default function UserNew(props: PageProps) {
+    const router = useRouter()
+    const [newUser] = useUserCreateMutation({})
+    const [autherLoaded, setAutherLoaded] = useState(false)
+    const [orgUID, setOrgUID] = useState("")
+
+    const form = useForm({
+        validate: yupResolver(schema),
+        initialValues: {
+            firstName: "",
+            lastName: "",
+            email: "",
+            phone: "",
+            orgUID: "",
+            roleID: "",
+            
+            departmentID: "",
+            orgName: "",
+            departmentName: "",
+            roleName: "",
+        },
+    })
+
+    // get org uid from local storage
+    useEffect(() => {
+        const obj = getObjectFromLocalStorage("org")
+        setOrgUID(obj.uid)
+        if (obj.uid != "" && obj.name) {
+            form.values.orgUID = obj.uid!
+            form.values.orgName = obj.name!
+        }
+    }, [orgUID, form])
+
+    // load auther
+    const authData = useAutherQuery()
+    if (authData.loading) {
+        return (
+            <PageLoader />
+        )
+    }
+    if (authData.error) {
+        showNotification({
+            disallowClose: false,
+            color: "red",
+            message: authData.error.message,
+        })
+        return <PageLoader isError={true} />
+    }
+    if (authData.data && !autherLoaded) {
+        if (!authData.data.auther.isAdmin) {
+            form.setValues({ orgUID: authData.data.auther.orgUID })
+        }
+        setAutherLoaded(true)
+    }
+
+    const handleOrgSelect = (item: Organization) => {
+        if (item) {
+            form.values.orgUID = item.uid!
+            form.values.orgName = item.name!
+        }
+    }
+
+    const handleDepartmentSelect = (item: Department | undefined) => {
+        if (item) {
+            form.values.departmentID = item?.id!
+            form.values.departmentName = item?.name!
+        }
+    }
+
+    const handleRoleSelect = (item: Role | undefined) => {
+        if (item) {
+            form.values.roleID = item?.id!
+            form.values.roleName = item?.name!
+        }
+    }
+
+    const handleSubmit = () => {
+        var newUserInput: UpdateUser = {
+            firstName: form.values.firstName,
+            lastName: form.values.lastName,
+            email: form.values.email,
+            phone: form.values.phone,
+            orgUID: form.values.orgUID,
+            roleID: form.values.roleID,
+        }
+
+        newUser({
+            variables: {input: newUserInput}
+        }).then((res: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "green",
+                message: `User ${res.data.userCreate.firstName} Created`,
+            })
+            router.push("/company/users")
+        }).catch((error: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "red",
+                message: error.message,
+            })
+        })
+    }
+
+    const handleCancel = () => {
+        router.push("/company/users")
+    }
+
+    return (
+        <UserNewHTML
+            title={props.title!}
+            auther={authData.data?.auther!}
+            orgUID={orgUID}
+            form={form}
+            handleSubmit={handleSubmit}
+            handleCancel={handleCancel}
+            handleOrgSelect={handleOrgSelect}
+            handleDepartmentSelect={handleDepartmentSelect}
+            handleRoleSelect={handleRoleSelect}
+        />
+    )
+}
