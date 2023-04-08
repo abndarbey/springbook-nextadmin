0d867e7d8f078907265f48c6bf093f957c3f2cd4
Tiger Darbey <tiger@pop-os.localdomain>
2023-04-08 11:45:55 +0530

    modify rack type new page

---
+++ src/modules/warehouses/rackType/RTNew/RTNewHTML.tsx
@@ -0,0 +1,75 @@
+import { Fragment, useState } from "react"
+
+import FormCard from "components/FormCard"
+import OrgSelectModal from "common/select-table/OrgSelectModal"
+import { Auther, Organization } from "@lib/generated/hooks"
+import { UseFormReturnType } from "@mantine/form"
+import { Textarea, TextInput } from "@mantine/core"
+
+interface IRackTypeFormValues {
+    name: string,
+    details: string,
+    storageType: string,
+    orgUID: string,
+
+    orgName: string,
+}
+
+interface IRackTypeNewHTML {
+    auther: Auther
+    orgUID: string
+    form: UseFormReturnType<IRackTypeFormValues>
+    handleSubmit: () => void
+    handleCancel: () => void
+    handleOrgSelect: (item: Organization) => void
+}
+
+export default function RackTypeNewHTML(props: IRackTypeNewHTML) {
+    const [orgModalOpened, setOrgModalOpened] = useState(false)
+
+    return (
+        <FormCard
+            submitButtonName="Create"
+            handleSubmit={props.form.onSubmit(props.handleSubmit)}
+            handleCancel={props.handleCancel}
+        >
+            {/* Select Organization */}
+            {props.auther.isAdmin && props.orgUID == "" &&
+                <Fragment>
+                    <OrgSelectModal
+                        opened={orgModalOpened}
+                        setOpened={setOrgModalOpened}
+                        handleSelect={props.handleOrgSelect}
+                    />
+                    <TextInput
+                        label="Organization"
+                        mb="md"
+                        placeholder="Select Organization"
+                        name="organization"
+                        onClick={() => setOrgModalOpened(true)}
+                        {...props.form.getInputProps("orgName")}
+                    />
+                </Fragment>
+            }
+            <TextInput
+                label="Name"
+                mb="md"
+                placeholder="Name"
+                {...props.form.getInputProps("name")}
+            />
+            <Textarea
+                label="Description"
+                placeholder="Description"
+                mb="md"
+                name="details"
+                {...props.form.getInputProps('details')}
+            />
+            <TextInput
+                label="Storage Type"
+                mb="md"
+                placeholder="Storage Type"
+                {...props.form.getInputProps("storageType")}
+            />
+        </FormCard>
+    )
+}
---
+++ src/modules/warehouses/rackType/RTNew/index.tsx
@@ -0,0 +1,125 @@
+import { useEffect, useState } from "react"
+import * as Yup from "yup"
+import { useRouter } from "next/router"
+import { useForm, yupResolver } from "@mantine/form"
+import { useRackTypeCreateMutation, UpdateRackType, Organization, useAutherQuery } from "@lib/generated/hooks"
+import { showNotification } from "@mantine/notifications"
+import { getObjectFromLocalStorage } from "common/localStorage"
+import PageLoader from "components/PageLoader"
+import { PageProps } from "types/types"
+import RTNewHTML from "./RTNewHTML"
+import { INavTrailProps } from "components/NavTrails"
+import Page from "components/Page"
+import PageHeader from "components/PageHeader"
+
+const schema = Yup.object().shape({
+    name: Yup.string().min(2, "Organization Name should have at least 2 letters"),
+    orgUID: Yup.string().min(2, "Invalid org UID"),
+})
+
+export default function RackTypeNew(props: PageProps) {
+    const router = useRouter()
+    const [newRackType] = useRackTypeCreateMutation({})
+    const [autherLoaded, setAutherLoaded] = useState(false)
+    const [orgUID, setOrgUID] = useState("")
+
+    const navTrails: INavTrailProps[] = [
+        { title: "Dashboard", href: "/" },
+        { title: "Rack Types", href: "/warehouses/rack-types" },
+        { title: "New", href: "#" },
+    ]
+
+    const form = useForm({
+        validate: yupResolver(schema),
+        initialValues: {
+            name: "",
+            storageType: "",
+            details: "",
+            orgUID: "",
+
+            orgName: "",
+        },
+    })
+
+    // get org uid from local storage
+    useEffect(() => {
+        const obj = getObjectFromLocalStorage("org")
+        setOrgUID(obj.uid)
+        if (obj.uid != "" && obj.name) {
+            form.values.orgUID = obj.uid!
+            form.values.orgName = obj.name!
+        }
+    }, [orgUID, form])
+
+    // load auther
+    const authData = useAutherQuery()
+    if (authData.loading) {
+        return (
+            <PageLoader />
+        )
+    }
+    if (authData.error) {
+        showNotification({
+            disallowClose: false,
+            color: "red",
+            message: authData.error.message,
+        })
+        return <PageLoader isError={true} />
+    }
+    if (authData.data && !autherLoaded) {
+        if (!authData.data.auther.isAdmin) {
+            form.setValues({ orgUID: authData.data.auther.orgUID })
+        }
+        setAutherLoaded(true)
+    }
+
+    const handleOrgSelect = (item: Organization) => {
+        if (item) {
+            form.values.orgUID = item.uid!
+            form.values.orgName = item.name!
+        }
+    }
+
+    const handleSubmit = () => {
+        var newRackTypeInput: UpdateRackType = {
+            name: form.values.name,
+            storageType: form.values.storageType,
+            orgUID: form.values.orgUID,
+        }
+
+        newRackType({
+            variables: {input: newRackTypeInput}
+        }).then((res: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "green",
+                message: `Rack Type ${res.data.rackTypeCreate.name} Created`,
+            })
+            router.push("/warehouses/rack-types")
+        }).catch((error: any) => {
+            showNotification({
+                disallowClose: false,
+                color: "red",
+                message: error.message,
+            })
+        })
+    }
+
+    const handleCancel = () => {
+        router.push("/warehouses/rack-types")
+    }
+
+    return (
+        <Page navTrails={navTrails} >
+        <PageHeader title={props.title!} />
+            <RTNewHTML
+                auther={authData.data?.auther!}
+                orgUID={orgUID}
+                form={form}
+                handleSubmit={handleSubmit}
+                handleCancel={handleCancel}
+                handleOrgSelect={handleOrgSelect}
+            />
+        </Page>
+    )
+}
