688ab52127e61a053bf67e5579766b4b00b02fa9
Tiger Darbey <tiger@pop-os.localdomain>
2023-04-21 14:44:55 +0530

    replace container table with pallet table in pallet list

--- src/modules/inventory/pallets/PalletList/index.tsx
+++ src/modules/inventory/pallets/PalletList/index.tsx
@@ -3,19 +3,19 @@ import Page from "components/Page"
 import PageHeader from "components/PageHeader"
 import { PageProps } from "types/types"
 import InventoryFilterBar from "components/InventoryFilterBar"
-import ContainerTable from "tables/inventory/ContainerTable"
+import PalletTable from "tables/inventory/PalletTable"
 
 const navTrails: INavTrailProps[] = [
     { title: "Dashboard", href: "/" },
-    { title: "Containers", href: "#" },
+    { title: "Pallets", href: "#" },
 ]
 
-export default function ContainerList(props: PageProps) {
+export default function PalletList(props: PageProps) {
     return (
         <Page navTrails={navTrails}>
             <PageHeader title={props.title!} />
             <InventoryFilterBar />
-            <ContainerTable />
+            <PalletTable />
         </Page>
     )
 }
