3ca033980321ee877f96b486c2b1c436d0369f20
Tiger Darbey <tiger@pop-os.localdomain>
2023-06-06 16:45:19 +0530

    update sidebar logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image'
 import { useMantineColorScheme } from '@mantine/core'
 
 // const logoSvg = require('../../assets/logos/slackbear_dark_logo.svg')
-const lightLogo = require('../../assets/logos/slackbear_logo.png')
-const darkLogo = require('../../assets/logos/slackbear_logo_dark.png')
+const lightLogo = require('../../assets/logos/springbook_logo.png')
+const darkLogo = require('../../assets/logos/springbook_logo_dark.png')
 
 export default function Logo() {
     const { colorScheme, toggleColorScheme } = useMantineColorScheme()
     return (
         <Image
-            width={'100'}
+            width={'130'}
             src={colorScheme === 'dark' ? darkLogo : lightLogo}
             alt="Random unsplash image"
         />
